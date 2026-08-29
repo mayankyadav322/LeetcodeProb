af691f31d788e3a0b377a0982e57d99456c87399
mayankyadav322 <mayankyadav32618@gmail.com>
2026-08-29 21:47:10 +0530

    Time: 12 ms (28.89%) | Memory: 44.8 MB (78.16%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n =g.size();
+        int m =s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int left =0;
+        int right =0;
+
+        while(left<n && right<m){
+            if(s[right] >= g[left]){
+                left = left + 1;
+            }
+            right = right + 1;
+        }
+        return left;
+    }
+};
