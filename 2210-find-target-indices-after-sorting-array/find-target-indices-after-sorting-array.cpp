d915953952fa3320a4cee70158334fa3b73f3da9
mayankyadav322 <mayankyadav32618@gmail.com>
2026-09-23 18:58:09 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (85.83%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
+
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == target) ans.push_back(i);
+        int low = 0;
+        int high = nums.size() - 1;
+        int first = -1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
         }
+
+        if(first != -1) {
+            while(first < nums.size() && nums[first] == target) {
+                ans.push_back(first);
+                first++;
+            }
+        }
+
         return ans;
     }
 };
